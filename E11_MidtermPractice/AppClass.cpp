6f030098444c066c92b7cad3575eaf98402e479e
Ashwin Ganapathiraju <axg3886@g.rit.edu>
2017-03-23 11:44:20 -0400

    Finished midterm practice - within time, even!

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -15,7 +15,15 @@ void AppClass::Update(void)
 	dTotalTime += dDeltaTime; //Incrementing the time differences 
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
+	static vector3 start = vector3();
+	static vector3 end = vector3(0.0f, 5.0f, 0.0f);
+	dTotalTime = fmod(dTotalTime, 10.0f); // Moduluo to every 10 seconds for lerp's sake
 	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+	m_m4Steve = glm::rotate(m_m4Steve, (float)dTotalTime * 72.0f, REAXISZ); // 2 laps around the center in 10 seconds - 60 * 60 / 100 * 2
+	m_m4Steve = glm::translate(m_m4Steve,
+		dTotalTime > 5.0 // if we're half way through
+		? glm::lerp(end, start, (float)((dTotalTime - 5.0f) / 5.0f)) // start lerping back down
+		: glm::lerp(start, end, (float)(dTotalTime / 5.0f))); // otherwise, lerp up, up, and away!
 #pragma endregion
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
